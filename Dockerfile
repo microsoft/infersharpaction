4dd2d423e311b3f5e7a00b12e96a9978c0e107f9
Xin Shi <xinshi@microsoft.com>
2020-05-12 12:32:30 -0700

    Filter report on changed files only (#2)
    * Filter on changed files only

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM xinshi/csharpcodeanalyzer:3679978-ga6caad564ac88f986c774eed4d826c80cc8f2704
+FROM xinshi/csharpcodeanalyzer:3721994-g1f43701f1f1b62a54e21600c4b3bf788082d64c2
 
 COPY entrypoint.sh /entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -8,9 +8,11 @@ sudo mkdir infer-out/captured
 sudo dotnet /app/Cilsil/bin/Debug/netcoreapp2.2/Cilsil.dll translate codeanalyzerinputbinaries --outcfg cfg.json --outtenv tenv.json
 sudo infer analyzejson --debug --cfg-json cfg.json --tenv-json tenv.json
 sudo chmod -R 777 infer-out/
+changes=$(echo $4 | sed 's/ /,/g')
+sudo dotnet /app/AnalysisResultParser/AnalysisResultParser/bin/Debug/netcoreapp2.2/AnalysisResultParser.dll infer-out/bugs.txt infer-out/filtered_bugs.txt $changes
 sudo dotnet /app/TelemetrySender/TelemetrySender/bin/Debug/netcoreapp2.2/TelemetrySender.dll GitHub $2 $3 infer-out/bugs.txt
 
-var="$( cat -s infer-out/bugs.txt | sed -E -e '/ANALYSIS_STOP|Bad_footprint|Missing_fld|PRECONDITION_NOT_MET|CONDITION_ALWAYS_FALSE|NULL_TEST_AFTER_DEREFERENCE|Assert_failure|CLASS_CAST_EXCEPTION|Abduction_case_not_implemented|Found.[0-9]+.issues|Summary.of.the.report/,/^[[:space:]]*$/d' )"
+var="$( cat infer-out/filtered_bugs.txt )"
 var="${var//'%'/'%25'}"
 var="${var//$'\n'/'%0A'}"
 var="${var//$'\r'/'%0D'}"
