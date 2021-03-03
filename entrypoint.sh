772b361abadfb1ecebe461ce972782f90925d206
Xin Shi <xinshi@microsoft.com>
2021-03-03 14:49:03 -0800

    Target Infer# v1.0 image (#33)
    * Target infer# v1.0 image
    
    * Fix a path
    
    * Update examples to .NET 5
    
    * Add a status badge

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,15 @@
 
 echo "Running Infer#..."
 sudo mkdir codeanalyzerinputbinaries
-sudo cp -r $1 /app/Cilsil/System.Private.CoreLib.dll codeanalyzerinputbinaries   
+sudo cp -r $1 /app/Cilsil/System.Private.CoreLib.dll codeanalyzerinputbinaries
+sudo dotnet /app/Cilsil/Cilsil.dll translate codeanalyzerinputbinaries --outcfg cfg.json --outtenv tenv.json
+echo -e "\e[1;33mYou may see 'Unable to parse instruction xxx' above. This is expected as we have not yet translated all the CIL instructions, which follow a long tail distribution. We are continuing to expand our .NET translation coverage. \e[0m\n"
+echo -e "Translation completed. Analyzing...\n"
 sudo infer capture
 sudo mkdir infer-out/captured
-sudo dotnet /app/Cilsil/Cilsil.dll translate codeanalyzerinputbinaries --outcfg cfg.json --outtenv tenv.json
-sudo infer analyzejson --debug --cfg-json cfg.json --tenv-json tenv.json
-sudo chmod -R 777 infer-out/
-reportonfiles=$(echo $2 | sed 's/ /,/g')
-sudo dotnet /app/AnalysisResultParser/AnalysisResultParser.dll infer-out/bugs.txt infer-out/filtered_bugs.txt $reportonfiles
+sudo infer $(infer help --list-issue-types 2> /dev/null | grep ':true:' | cut -d ':' -f 1 | sed -e 's/^/--disable-issue-type /') --enable-issue-type NULL_DEREFERENCE --enable-issue-type DOTNET_RESOURCE_LEAK analyzejson --debug --cfg-json cfg.json --tenv-json tenv.json
 
-var="$( cat infer-out/filtered_bugs.txt )"
+var="$( cat infer-out/report.txt )"
 var="${var//'%'/'%25'}"
 var="${var//$'\n'/'%0A'}"
 var="${var//$'\r'/'%0D'}"
