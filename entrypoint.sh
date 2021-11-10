5699518c9d011c791a7cf66100d8cd49d0929c8b
Xin Shi <xinshi@microsoft.com>
2021-11-10 10:34:05 -0800

    Support path with spaces

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 curl -o run_infersharp.sh https://raw.githubusercontent.com/microsoft/infersharp/v1.2/run_infersharp_ci.sh
 chmod +x run_infersharp.sh
-./run_infersharp.sh $1
+./run_infersharp.sh "$1"
 
 printf '\n\nAnalysis Result\n'
 printf '%50s\n' | tr ' ' =
