996eb92818febf17e5f7fd23e01a075b59466dd0
Xin Shi <xinshi@microsoft.com>
2021-11-29 15:05:52 -0800

    Fix ASCII

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-curl -o run_infersharp.sh https://raw.githubusercontent.com/microsoft/infersharp/v1.1/run_infersharp_ci.sh
+curl -o run_infersharp.sh https://raw.githubusercontent.com/microsoft/infersharp/v1.2/run_infersharp_ci.sh
 chmod +x run_infersharp.sh
 ./run_infersharp.sh $1
 
 var="$( cat infer-out/report.txt )"
+var="${var//'#'/'%23'}"
 var="${var//'%'/'%25'}"
 var="${var//$'\n'/'%0A'}"
 var="${var//$'\r'/'%0D'}"
