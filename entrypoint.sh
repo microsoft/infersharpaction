6c6e4209cc512886791ab1a97c2262895f521079
Xin Shi <xinshi@microsoft.com>
2021-11-29 14:20:57 -0800

    Debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 curl -o run_infersharp.sh https://raw.githubusercontent.com/microsoft/infersharp/v1.2/run_infersharp_ci.sh
 chmod +x run_infersharp.sh
 ./run_infersharp.sh "$1"
+cat infer-out/report.txt
 
 var="$(cat infer-out/report.txt)"
 echo "::set-output name=output_txt::$var"
