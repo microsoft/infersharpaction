3eb2042284379579450be5b75fc2af4729fc040e
Xin Shi <xinshi@microsoft.com>
2021-11-29 14:18:15 -0800

    cat output

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ curl -o run_infersharp.sh https://raw.githubusercontent.com/microsoft/infersharp
 chmod +x run_infersharp.sh
 ./run_infersharp.sh "$1"
 
-echo "::set-output name=output_txt::$(cat infer-out/report.txt)"
-echo "::set-output name=output_sarif::$(cat infer-out/report.sarif)"
+var="$(cat infer-out/report.txt)"
+echo "::set-output name=output_txt::$var"
