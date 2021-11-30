e10c8a7fa391aa8bf64a2869c5331142f381492d
Xin Shi <xinshi@microsoft.com>
2021-11-29 21:24:33 -0800

    Point to updated script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -o run_infersharp.sh https://raw.githubusercontent.com/microsoft/infersharp/v1.2/run_infersharp_ci.sh
+curl -o run_infersharp.sh https://raw.githubusercontent.com/microsoft/infersharpaction/xinshi/outputsarif/run_infersharp_ci.sh
 chmod +x run_infersharp.sh
 ./run_infersharp.sh "$1"
 
