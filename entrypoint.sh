59791164ec38ef02a6cd399e803687e010cfe1d9
Xin Shi <xinshi@microsoft.com>
2022-10-04 13:24:07 -0700

    Add .inferconfig

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,9 @@
 set -e
 
 curl -o run_infersharp.sh https://raw.githubusercontent.com/microsoft/infersharpaction/xinshi/v1.4/run_infersharp_ci.sh
+curl -o .inferconfig https://raw.githubusercontent.com/microsoft/infersharp/v1.4/.inferconfig
 chmod +x run_infersharp.sh
+chmod +x .inferconfig
 ./run_infersharp.sh "$1" $2
 
 results="$( cat infer-out/report.txt )"
