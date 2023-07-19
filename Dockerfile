04cdd6c10b1acf086aa0a1b393019f0680af3bce
workgroupengineering <workgroupengineering@users.noreply.github.com>
2023-07-19 20:58:33 +0200

    feat: GitHub SARIF (#56)
    * feat: Add flag that allow compatibility with GitHub SARIF
    
    * update infersharp v1.5

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
-FROM mcr.microsoft.com/infersharp:v1.4
+FROM mcr.microsoft.com/infersharp:v1.5
 
+ADD run_infersharp_ci.sh /run_infersharp_ci.sh
 COPY entrypoint.sh /entrypoint.sh
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,16 @@
 
 set -e
 
-curl -o run_infersharp.sh https://raw.githubusercontent.com/microsoft/infersharpaction/v1.4/run_infersharp_ci.sh
-curl -o .inferconfig https://raw.githubusercontent.com/microsoft/infersharp/v1.4/.inferconfig
-chmod +x run_infersharp.sh
-chmod +x .inferconfig
-./run_infersharp.sh "$1" $2
+# transform binary-path from relative tu absolute
+SCRIPT=$(realpath -s "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+FULLPATH=$(readlink -f "$1") 
+
+# Downlaod infersharp config
+curl -o $SCRIPTPATH/.inferconfig https://raw.githubusercontent.com/microsoft/infersharp/v1.5/.inferconfig
+
+# Add execution auth
+chmod +x $SCRIPTPATH/run_infersharp_ci.sh
+chmod +x $SCRIPTPATH/.inferconfig
+
+$SCRIPTPATH/run_infersharp_ci.sh "$FULLPATH" $2 $3
